d8db79f61c437cf78b1ba43c6eebc4632fa3287b
Peter Jonsson <95jonpet@gmail.com>
2017-02-19 11:40:23 +0100

    Added documentation for new class

--- src/se/peterjonsson/flocking/Position.java
+++ src/se/peterjonsson/flocking/Position.java
@@ -1,16 +1,44 @@
 package se.peterjonsson.flocking;
 
+/**
+ * Represents the position of an object, for example an {@link Agent} or an {@link Obstacle}.
+ *
+ * @author Peter Jonsson <95jonpet@gmail.com>
+ */
 class Position {
+    /**
+     * Horizontal position.
+     */
     final double x;
+
+    /**
+     * Vertical position.
+     */
     final double y;
+
+    /**
+     * Angle in radians.
+     */
     final double angle;
 
+    /**
+     * Creates a new position.
+     * @param x Horizontal coordinate.
+     * @param y Vertical coordinate.
+     * @param angle Angle in radians.
+     */
     Position(final double x, final double y, final double angle) {
         this.x = x;
         this.y = y;
         this.angle = angle;
     }
 
+    /**
+     * Creates a new position.
+     * @see #Position(double, double, double)
+     * @param x Horizontal coordinate.
+     * @param y Vertical coordinate.
+     */
     Position(final double x, final double y) {
         this(x, y, 0);
     }
